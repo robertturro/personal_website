2bf450b238a9fd21615afa9907feba6fca4dbd40
Robert Turro <robturro@gmail.com>
2024-05-28 19:50:04 -0400

    5/28/2024

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("submit_question/", views.CreateQuestion.as_view(), name="question"),
+    path("chat/", views.CreateQuestion.as_view(), name="question"),
     path("new_page", views.CreateLinkClick.as_view(), name="link"),
 ]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,55 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import "../styles/homepage.css";
+import MyImage from "../styles/github_pic.PNG";
+
 function Home() {
-  return <div>Home</div>;
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    getResponse();
+  }, []);
+
+  const getResponse = () => {
+    api
+      .get("/api/chat/")
+      .then((res) => res.data)
+      .then((data) => {
+        setResponse(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const addLink = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/new_page/", { linkname })
+      .then((res) => {
+        if (res.status === 201) alert("Link Clicked");
+        else alert("Failed to go to page.");
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2 className="header">Robert Turro</h2>
+        <h1 className="subtitle">
+          Full-Stack Developer with Strong Machine Learning and Data Science
+          Skills
+        </h1>
+        <img
+          src={MyImage}
+          alt="My Image"
+          width="500"
+          height="600"
+          color="black"
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
