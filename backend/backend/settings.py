4f5ef8afd7a93e61949584710707ff3efb805a86
Robert Turro <robturro@gmail.com>
2025-01-20 13:05:21 -0500

    AWS CI CD

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ["secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
