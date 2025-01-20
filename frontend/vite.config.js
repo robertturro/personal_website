28b92e922f44a4d304865d65375c3fa331cd4fae
Robert Turro <robturro@gmail.com>
2025-01-20 15:04:51 -0500

    AWS CI CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/static/",
   plugins: [react()],
   css: {
     localConvention: "camelCase",
