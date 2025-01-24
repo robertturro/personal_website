c21f82c2a0256d136aabef1c0bba5e4c94452d2f
Robert Turro <robturro@gmail.com>
2025-01-23 21:52:42 -0500

    AWS CI CD

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 //const apiUrl = "/choreo-apis/personalwebsite/backend/v1.0";
-const apiUrl = "http://localhost:8000";
+//const apiUrl = "http://localhost:8000";
 //const apiUrl = "ec2-3-129-6-154.us-east-2.compute.amazonaws.com/";
-//const apiUrl = "http://ec2-3-129-6-154.us-east-2.compute.amazonaws.com/";
+const apiUrl = "http://ec2-3-129-6-154.us-east-2.compute.amazonaws.com/";
 //ec2-3-129-6-154.us-east-2.compute.amazonaws.com
 
 console.log("HERE");
