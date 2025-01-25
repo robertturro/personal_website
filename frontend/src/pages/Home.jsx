bafeed2883c5725c631e8a79461045342d74db42
Robert Turro <robturro@gmail.com>
2025-01-25 16:40:04 -0500

    AWS CI CD

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ import { Contact } from "../components/Contact/Contact";
 
 function Home() {
   const [response, setResponse] = useState(
-    "ROB-BOT's response will be displayed here."
+    "ROB-BOT's response will be displayed here xxx."
   );
   const [question, setQuestion] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
