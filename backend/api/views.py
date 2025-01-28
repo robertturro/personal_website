1128c93b2921948b04b12301c68a9806d25c06e2
Robert Turro <robturro@gmail.com>
2025-01-27 20:46:21 -0500

    AWS CI CD 10

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .serializers import UserSerializer, QuestionSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Question
-#from .rag import *
+from .rag import *
 from datetime import date
 import json
 from django.http import JsonResponse, HttpResponse, StreamingHttpResponse
@@ -28,14 +28,13 @@ class CreateQuestion(generics.ListCreateAPIView):
         data = json.loads(request.body)
         question = data.get('question')
 
-        #context = get_context(question)
-        #prompt = create_prompt(context, question)
-        #thread = create_thread()
-        #response = get_response(prompt, thread)
+        context = get_context(question)
+        prompt = create_prompt(context, question)
+        thread = create_thread()
+        response = get_response(prompt, thread)
 
         #q = Question(question=question, date=date.today(), answer=response)
         #q.save()
-        response = question
 
         return JsonResponse({'response': response})
         
